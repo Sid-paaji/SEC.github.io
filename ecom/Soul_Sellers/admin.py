ba46d7cc5a27685cee562fbb4673595b4c8240d5
Sid-paaji <siddharthchoudhary475@gmail.com>
2023-10-16 13:06:18 +0530

    Update admin.py

--- ecom/Soul_Sellers/admin.py
+++ ecom/Soul_Sellers/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import User,Category,Shoe,ShoppingCart,UserProfile
+admin.site.register(User)
+admin.site.register(Shoe)
+admin.site.register(ShoppingCart)
+admin.site.register(UserProfile)
+admin.site.register(Category)
+
 
 # Register your models here.
